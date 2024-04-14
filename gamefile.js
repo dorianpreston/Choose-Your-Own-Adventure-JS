1999672fed7d57613231cf13c5905b1ea0048e66
dorianpreston <166086065+dorianpreston@users.noreply.github.com>
2024-04-13 19:59:12 -0700

    Update gamefile.js
    Text display property added

--- gamefile.js
+++ gamefile.js
@@ -38,6 +38,7 @@ button2.onclick= goCave;
 button3.onclick= fightDragon;
 
 function goStore() {
+    displaytext.innerText = "You enter the store."
   button1.innerText = "Buy 10 health (10 gold)";
   button2.innerText = "Buy weapon (30 gold)";
   button3.innerText = "Go to town square";
