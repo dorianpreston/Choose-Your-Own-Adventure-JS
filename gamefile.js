7ab3c417219c7a3ba84c00657654686c763f3202
dorianpreston <166086065+dorianpreston@users.noreply.github.com>
2024-04-13 18:54:24 -0700

    Update gamefile.js
    goStore function 3 buttons defined

--- gamefile.js
+++ gamefile.js
@@ -36,3 +36,9 @@ function fightDragon () {
 button1.onclick = goStore;
 button2.onclick= goCave;
 button3.onclick= fightDragon;
+
+function goStore() {
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+}
