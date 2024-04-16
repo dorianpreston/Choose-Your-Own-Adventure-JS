e479c9041b69b3fb8b1d0904a725e78e773111bc
dorianpreston <166086065+dorianpreston@users.noreply.github.com>
2024-04-16 01:32:11 -0700

    Update gamefile.js
    defined button, button functions and text variables in a const statement

--- gamefile.js
+++ gamefile.js
@@ -70,6 +70,8 @@ button3.onclick = goTown;
 const locations = [
   {
     name: "town square",
-    "button text": ["Go to store", "Go to cave", "Fight dragon"]
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon]
+    text: "You are in the town square. You see a sign that says \"Store\".
   }
 ];
